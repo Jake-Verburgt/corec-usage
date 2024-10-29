4deb13410bc0d5f89706ad6d41f1e48d5b1114b6
jake <jakeverburgt@gmail.com>
2024-10-29 19:53:17 -0400

    Actually added old data and updated readme and started analysis

--- README.md
+++ README.md
@@ -10,10 +10,23 @@ This historical data was really nice for deciding what time of day to plan a tri
 2) Appends any new data to a database
 3) Runs data analysis and visualization to guide my "when to go to corec" decisions.
 
+## Data Format
+Data is stored in a single sqlite table titled ```corec_usage``` under ```./data_public/corec_usage.db``` with the following columns:
+
+
+|    |   id |   LocationId |   TotalCapacity | LocationName               | LastUpdatedDateAndTime   |   LastCount |   FacilityId |   IsClosed |
+|---:|-----:|-------------:|----------------:|:---------------------------|:-------------------------|------------:|-------------:|-----------:|
+|  0 |    1 |         6129 |              10 | Upper Track (4th Floor)    | 2024-10-13T20:26:16.17   |           0 |          976 |          0 |
+|  1 |    2 |         5978 |              20 | Faculty/Staff Workout Area | 2024-10-13T20:26:16.827  |           1 |          976 |          0 |
+|  2 |    3 |         5995 |              30 | Upper Fitness              | 2024-10-13T20:26:17.45   |           5 |          976 |          0 |
+|  3 |    4 |         5996 |              75 | Upper Gym                  | 2024-10-13T20:26:18.077  |          45 |          976 |          0 |
+|  4 |    5 |         7451 |              30 | MP6                        | 2024-10-13T20:26:18.78   |          20 |          976 |          0 |
+
+
 # How to use:
 ## 1) Install Dependencies:
 
-```pandas```, ```sqlite3```, ```sqalchemy```, ```requests```, ```matplitlib```, ```seaborn```
+```pandas```, ```sqlite3```, ```sqalchemy```, ```requests```, ```matplitlib```, ```seaborn```, ```tabulate```
 ### Install conda environment from file
  * ```conda install -f environment.yaml```
 ## 2) Set Up Cron Job to collect data
@@ -24,7 +37,7 @@ To Set up a CRON Job to run every 30 minutes, you need to edit the "cron table"
     * It will likely open in the vim or nano editor (two terminal based editors). You can change this by running the ```select-editor``` command, and selecting a different editor
 * At the bottom of the file, add the following line: ```*/30 * *   *   *    /path/to/corec-usage/bin/python /path/to/corec_scrape.py```
     * MAKE SURE YOU ALTER THE PYTHON AND FILE PATHS
-    * In the cron table, the prder goes "minutes, hours, dom, month, dow, command" The lie above is just saying "Every 30 minutes of every hour of every day, run this command"
-    * If successful, you will see a 
+    * In the cron table, the prder goes "minutes, hours, dom, month, dow, command" The line above is just saying "Every 30 minutes of every hour of every day, run this command"
+
 ## 3) Run analysis on data
 Data Analysis is ran in a n
--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 import os
 
 
-def read_db(sqlite_file: str ="/home/jake/scripts/workshop/corec-usage/data_public/corec_usage.db", 
+def read_db(sqlite_file: str ="./data_public/corec_usage.db", 
                table_name: str ="corec_usage",
                start_range:dt= dt.strptime("1970-01-01T00:00:00.000",'%Y-%m-%dT%H:%M:%S.%f'),
                end_range:dt = dt.now()) -> pd.DataFrame:
@@ -26,29 +26,42 @@ def read_db(sqlite_file: str ="/home/jake/scripts/workshop/corec-usage/data_publ
     
     #Process data types and add date data
     existing_data["LastUpdatedDateAndTime"] = pd.to_datetime(existing_data.LastUpdatedDateAndTime) #Convert string column to datetime object
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     existing_data["day_name"] = existing_data["LastUpdatedDateAndTime"].dt.day_name() # Extract day of the week
+    existing_data["day_name"] = pd.Categorical(existing_data.day_name,categories=days_of_week, ordered = True) # Make the days a proper categorical variable
     existing_data["day_of_week"] = existing_data.LastUpdatedDateAndTime.dt.dayofweek #Get Day 0f week (0-7)
     existing_data["hour_of_day"] = existing_data.LastUpdatedDateAndTime.dt.round("h").dt.hour #Get nearest hour (0-23)
     existing_data["hour_of_day"] = existing_data["hour_of_day"].replace(0, 24)
+    existing_data["date"] = existing_data["LastUpdatedDateAndTime"].dt.date #Extract date
     existing_data["IsClosed"] = existing_data.IsClosed.astype(bool)
+
+    existing_data.sort_values(by="LastUpdatedDateAndTime", inplace=True)
 
     return existing_data
 
 
 
-#Dump relevant timestamp information
-# corec_df["LastUpdatedDateAndTime"] = corec_df.LastUpdatedDateAndTime.apply(pd.to_datetime) #Convert to datetime object
-# corec_df["day"] = corec_df["LastUpdatedDateAndTime"].dt.day_name() # Extract day of the week
-# corec_df["day"] = pd.Categorical(corec_df["day"],categories=days_of_week, ordered = True) # Make the days a proper categorical variable
-# corec_df["time"] = corec_df["LastUpdatedDateAndTime"].dt.time #Extract time of day
-# corec_df["date"] = corec_df["LastUpdatedDateAndTime"].dt.date #Extract date
-# corec_df["hour"] = corec_df["LastUpdatedDateAndTime"].dt.round('H').dt.hour #Extract nearest hour
-# corec_df["hour"] = corec_df["hour"].replace(0, 24)
-# corec_df.head()
+
+
 
 def main():
     data:pd.DataFrame = read_db()
-    print(data)
+    day_hour_averages = data.groupby(["LocationId", "day_name", "hour_of_day"]).mean().reset_index().sort_values(["day_name", "hour_of_day"])
+    
+    day_hour_averages = day_hour_averages.pivot(index="day_name", columns=["LocationId", "hour_of_day"], values="LastCount")
+    day_hour_averages.fillna(0.0, inplace = True)
+
+    ids_to_name = data[["LocationId", "LocationName"]].drop_duplicates().set_index("LocationId")["LocationName"].to_dict()
+
+    print(day_hour_averages[5976])
+
+
+# fig, axes = plt.subplots(len(relevant_areas), 1, figsize=(20, 4*len(relevant_areas)), sharex=True, facecolor="white")
+# for area, ax in zip(relevant_areas, axes):
+#     occupancy_data = day_hour_averages[area]
+#     ax.set_title(area, fontsize=16)
+#     sns.heatmap(occupancy_data, ax=ax)
+#     ax.set_ylabel(None)
 
 
 if __name__ == "__main__":
